e6a2fee83c4ac27f86ef376656d150a74e518bdb
rnoguer22 <rnogugon@myuax.com>
2023-11-20 21:20:20 +0100

    Añadimos las listas desplegables

--- Ejercicio1/interface.py
+++ Ejercicio1/interface.py
@@ -16,4 +16,42 @@ class MenuPizza(QWidget):
 
         self.iniciar_gui()
     
-    
+    def iniciar_gui(self):
+        layout_vertical = QVBoxLayout()
+        layout_horizontal1 = QHBoxLayout()
+        layout_horizontal2 = QHBoxLayout()
+        layout_horizontal3 = QHBoxLayout()
+
+        label = QLabel("Seleccione su menu: ")
+        layout_horizontal1.addWidget(label)
+
+        # Lista desplegable para seleccionar el entrante
+        box_entrantes = QComboBox(self)
+        box_entrantes.addItems(self.entrantes_disponibles)
+        layout_horizontal2.addWidget(box_entrantes)        
+        
+        # Lista desplegable para seleccinar la pizza
+        box_pizzas = QComboBox(self)
+        box_pizzas.addItems(self.pizzas_disponibles)
+        layout_horizontal2.addWidget(box_pizzas)
+
+        # Lista desplegable para seleccinar la bebida
+        box_bebidas = QComboBox(self)
+        box_bebidas.addItems(self.bebidas_disponibles)
+        layout_horizontal2.addWidget(box_bebidas)
+
+        # Lista desplegable para seleccinar los postres
+        box_postres = QComboBox(self)
+        box_postres.addItems(self.postres_disponibles)
+        layout_horizontal2.addWidget(box_postres)
+
+        
+
+
+        layout_vertical.addLayout(layout_horizontal1)
+        layout_vertical.addLayout(layout_horizontal2)
+        layout_vertical.addLayout(layout_horizontal3)
+
+        self.setLayout(layout_vertical)
+
+        self.show()
