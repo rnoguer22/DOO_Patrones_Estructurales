9e31945a4061aa77398000df4bd1c26a3e293d2f
rnoguer22 <rnogugon@myuax.com>
2023-11-20 21:30:06 +0100

    Vamos a soluxionar errores de la interfaz :)))

--- Ejercicio1/interface.py
+++ Ejercicio1/interface.py
@@ -1,6 +1,5 @@
-import typing
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout,QComboBox, QPushButton, QMessageBox, QTextEdit
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout,QComboBox, QPushButton, QMessageBox, QTextEdit
 
 class MenuPizza(QWidget):
 
@@ -45,7 +44,21 @@ class MenuPizza(QWidget):
         box_postres.addItems(self.postres_disponibles)
         layout_horizontal2.addWidget(box_postres)
 
-        
+        # Boton para confirmar la seleccion
+        boton_confirmar = QPushButton("Confirmar")
+        layout_horizontal3.addWidget(boton_confirmar)
+        boton_confirmar.clicked.connect(self.confirmar_seleccion)
+
+        # Boton para cancelar la seleccion
+        boton_cancelar = QPushButton("Cancelar")
+        layout_horizontal3.addWidget(boton_cancelar)
+        boton_cancelar.clicked.connect(self.cancelar_seleccion)
+
+        # Área de texto para mostrar la orden
+        self.text_area = QTextEdit(self)
+        self.text_area.setReadOnly(True)
+        self.text_area.setMinimumSize(400, 300) #Hacemos que el area de texto sea un poco mas grande
+        layout_horizontal3.addWidget(self.text_area)
 
 
         layout_vertical.addLayout(layout_horizontal1)
@@ -55,3 +68,29 @@ class MenuPizza(QWidget):
         self.setLayout(layout_vertical)
 
         self.show()
+    
+
+
+    def confirmar_seleccion(self):
+        self.seleccion = []
+        for i in range(self.layout().count()):
+            widget = self.layout().itemAt(i).widget()
+            if isinstance(widget, QComboBox):
+                self.seleccion.append(widget.currentText())
+        print(self.seleccion)
+        #Si la orden es correcta, la guardamos en una variable
+        self.orden = ", ".join(self.seleccion)
+        #Mostramos la orden en la interfaz
+        self.text_area.append(f"Tu orden es: {self.orden}")
+        #QMessageBox.about(self, "Confirmacion", "Su menu ha sido confirmado")
+        self.close()
+    
+    def cancelar_seleccion(self):
+        self.seleccion = []
+        QMessageBox.about(self, "Cancelacion", "Su menu ha sido cancelado")
+        self.close()
+
+app = QApplication(sys.argv)
+ventana = MenuPizza()
+ventana.show()
+app.exec_()  
