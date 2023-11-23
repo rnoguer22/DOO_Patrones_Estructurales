2a0c34f1d8a9ddf64a2af56080eb88241fc66669
rnoguer22 <rnogugon@myuax.com>
2023-11-23 12:20:12 +0100

    Organizacion de carpetas para el segundo ejercicio

---
+++ Ejercicio2/lanzador.py
@@ -0,0 +1,32 @@
+from Proxy.subject import Subject
+from Proxy.realSubject import RealSubject
+from Proxy.proxy import Proxy
+
+class Lanzador:
+
+    def lanzar(self):
+        def client_code(subject: Subject) -> None:
+            """
+            The client code is supposed to work with all objects (both subjects and
+            proxies) via the Subject interface in order to support both real subjects
+            and proxies. In real life, however, clients mostly work with their real
+            subjects directly. In this case, to implement the pattern more easily, you
+            can extend your proxy from the real subject's class.
+            """
+
+            # ...
+
+            subject.request()
+
+            # ...
+
+
+        print("Client: Executing the client code with a real subject:")
+        real_subject = RealSubject()
+        client_code(real_subject)
+
+        print("")
+
+        print("Client: Executing the same client code with a proxy:")
+        proxy = Proxy(real_subject)
+        client_code(proxy)
---
+++ Ejercicio2/main.py
@@ -0,0 +1,6 @@
+from lanzador import Lanzador
+
+if __name__ == '__main__':
+
+    lanzador = Lanzador()
+    lanzador.lanzar()
