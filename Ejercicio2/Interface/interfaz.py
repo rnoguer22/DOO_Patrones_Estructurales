e0d4d60e1ce1d2fbe95e2acd251118459c97b4f1
rnoguer22 <rnogugon@myuax.com>
2023-11-24 12:01:59 +0100

    Estructura basica del registro de usuarios

--- Ejercicio2/Interface/interfaz.py
+++ Ejercicio2/Interface/interfaz.py
@@ -4,6 +4,8 @@ from PyQt5.QtWidgets import QWidget, QApplication, QMessageBox, QPushButton
 class Interface(QWidget):
 
     def __init__(self):
+        super().__init__()
+
         dialogo = QMessageBox()
         dialogo.setWindowTitle("Inicio")
         dialogo.setText("¿Deseas registrarte o ya dispones de una cuenta?")
@@ -13,9 +15,9 @@ class Interface(QWidget):
         inicio_sesion = QPushButton("Iniciar sesion")
         dialogo.addButton(registro, QMessageBox.YesRole)
         dialogo.addButton(inicio_sesion, QMessageBox.NoRole)
-
-        dialogo.exec_()
         
+        dialogo.exec_()
+
         # Verificar qué botón fue presionado
         if dialogo.clickedButton() == registro:
             self.registrarse()
@@ -24,12 +26,39 @@ class Interface(QWidget):
 
 
     def registrarse(self):
-        print('Con este metodo realizaremos el registro')
-    
+        layout_vertical = QVBoxLayout()
+        layout_horizontal1 = QHBoxLayout()
+        layout_horizontal2 = QHBoxLayout()
+
+        label = QLabel("Introduce tu nombre de usuario: ")
+        layout_horizontal1.addWidget(label)
+        # Campo de texto para introducir el nombre de usuario
+        self.nombre_usuario = QLineEdit()
+        layout_horizontal1.addWidget(self.nombre_usuario)
+
+        label_contrasena = QLabel("Introduce tu contraseña: ")
+        layout_horizontal2.addWidget(label_contrasena)
+        # Campo de texto para introducir la contraseña
+        self.contrasena = QLineEdit()
+        self.contrasena.setEchoMode(QLineEdit.Password)
+        layout_horizontal2.addWidget(self.contrasena)
+
+        # Botón para confirmar el registro
+        boton_confirmar = QPushButton("Confirmar")
+        boton_confirmar.onClick.connect(confirmar_registro)
+        layout_horizontal3.addWidget(boton_confirmar)
+
+        # Botón para cancelar el registro
+        boton_cancelar = QPushButton("Cancelar")
+        boton_cancelar.onClick.connect(cancelar_registro)
+        layout_horizontal3.addWidget(boton_cancelar)
+
+
+
     def iniciar_sesion(self):
         print('Con este metodo iniciaremos sesion')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ventana = Interface()
-    sys.exit(app.exec_())
+    app.exec_()
