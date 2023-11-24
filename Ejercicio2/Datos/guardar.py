256843df786fff0e5be4399f80b3ab3c8c13a67a
rnoguer22 <rnogugon@myuax.com>
2023-11-24 14:50:34 +0100

    Añadimos un metodo para guardar los datos con id

--- Ejercicio2/Datos/guardar.py
+++ Ejercicio2/Datos/guardar.py
@@ -7,5 +7,19 @@ class Guardar:
 
     def guardar(self, datos):
         with open(self.ruta, 'a') as csv_file:
-            writer = csv.writer(csv_file)
+            writer = csv.writer(csv_file)    
+            id = self.obtener_ultimo_id(self.ruta)
+            datos.insert(0, id) #Insertamos el id del menu en la primera posicion de la lista
             writer.writerow(datos)
+
+    # Función para obtener el último ID del archivo CSV
+    def obtener_ultimo_id(self):
+        try:
+            with open(self.ruta, 'r', newline='') as archivo:
+                lector_csv = csv.reader(archivo)
+                lineas = list(lector_csv)
+                return len(lineas)
+        except:
+            return 1  # Si el archivo no existe, el primer ID será 1
+        
+    
