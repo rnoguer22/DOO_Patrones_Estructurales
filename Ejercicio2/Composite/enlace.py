7340a94c188694c1b7682e41cff72011fce80834
rnoguer22 <rnogugon@myuax.com>
2023-11-25 22:46:26 +0100

    Clase enlace

---
+++ Ejercicio2/Composite/enlace.py
@@ -0,0 +1,18 @@
+from documentosSistema import DocumentoSistema
+
+class Enlace(DocumentoSistema):
+    def __init__(self, nombre, destino):
+        self.nombre = nombre
+        self.destino = destino
+
+    def getNombre(self):
+        return self.nombre
+
+    def getTipo(self):
+        return "Enlace"
+
+    def getTamanio(self):
+        return 0
+
+    def acceder(self, usuario):
+        print(f"Acceso al enlace '{self.nombre}' por '{usuario}', redirigiendo a '{self.destino.obtener_nombre()}'")
