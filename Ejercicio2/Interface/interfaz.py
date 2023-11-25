0603e7a67112d4326880e06c86a297f4233447ce
rnoguer22 <rnogugon@myuax.com>
2023-11-25 13:15:06 +0100

    Mejoramos el registro e incio de sesion

--- Ejercicio2/Interface/interfaz.py
+++ Ejercicio2/Interface/interfaz.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 from PyQt5.QtWidgets import QWidget, QApplication, QMessageBox, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit
 from Datos.guardar import Guardar
 
@@ -27,24 +28,27 @@ class Interface(QWidget):
             self.iniciar_sesion()
 
 
-    def registrarse(self):
+    def ventana(self, titulo):
+        self.titulo = titulo
         layout_vertical = QVBoxLayout()
         layout_horizontal1 = QHBoxLayout()
-        layout_horizontal2 = QHBoxLayout()
+        self.layout_horizontal2 = QHBoxLayout()
         layout_horizontal3 = QHBoxLayout()
 
         label = QLabel("Introduce tu nombre de usuario: ")
         layout_horizontal1.addWidget(label)
         # Campo de texto para introducir el nombre de usuario
         self.nombre_usuario = QLineEdit()
+        self.nombre_usuario.setPlaceholderText("usuario")
         layout_horizontal1.addWidget(self.nombre_usuario)
 
         label_contrasena = QLabel("Introduce tu contraseña: ")
-        layout_horizontal2.addWidget(label_contrasena)
+        self.layout_horizontal2.addWidget(label_contrasena)
         # Campo de texto para introducir la contraseña
         self.contrasena = QLineEdit()
+        self.contrasena.setPlaceholderText("contraseña")
         self.contrasena.setEchoMode(QLineEdit.Password)
-        layout_horizontal2.addWidget(self.contrasena)
+        self.layout_horizontal2.addWidget(self.contrasena)
 
         # Botón para confirmar el registro
         boton_confirmar = QPushButton("Confirmar")
@@ -57,22 +61,33 @@ class Interface(QWidget):
         layout_horizontal3.addWidget(boton_cancelar)
 
         layout_vertical.addLayout(layout_horizontal1)
-        layout_vertical.addLayout(layout_horizontal2)
+        layout_vertical.addLayout(self.layout_horizontal2)
         layout_vertical.addLayout(layout_horizontal3)
 
         self.setLayout(layout_vertical)
-        self.setWindowTitle("Registro")
+        self.setWindowTitle(titulo)
         self.show()
+
 
     def confirmar_registro(self):
         datos = [self.nombre_usuario.text(), self.contrasena.text()]
-        # Guardamos los datos en el archivo CSV
-        guardar = Guardar('Ejercicio2/Datos/usuarios.csv')
-        guardar.guardar(datos)
-        # Mostramos un mensaje de confirmación
-        QMessageBox.information(self, "Registro", "Registro completado correctamente")
+        if self.titulo == 'Registrarse':
+            if datos[0] == '' or datos[1] == '':
+                QMessageBox.warning(self, "Registro", "Por favor, introduce usuario y contraseña")
+                return False
+            elif datos[1] != self.repetir_contrasena.text():
+                QMessageBox.warning(self, "Registro", "Las contraseñas no coinciden")
+                return False
+            # Guardamos los datos en el archivo CSV
+            guardar = Guardar('Ejercicio2/Datos/usuarios.csv')
+            guardar.guardar(datos)
+            # Mostramos un mensaje de confirmación
+            QMessageBox.information(self, "Registro", "Registro completado correctamente")
+        elif self.titulo == 'Iniciar sesión':
+            self.verificar_credenciales(datos[0], datos[1])
         self.close()
         
+
     def cancelar_registro(self):
         self.nombre_usuario.setText("")
         self.contrasena.setText("")
@@ -80,5 +95,36 @@ class Interface(QWidget):
         self.close()
 
 
+    def verificar_credenciales(self, user, password):
+        if user == '' or password == '':
+            QMessageBox.warning(self, "Inicio de sesión", "Por favor, introduce usuario y contraseña")
+            return False
+        try:
+            with open('Ejercicio2/Datos/usuarios.csv', 'r', newline='') as archivo:
+                lector_csv = csv.reader(archivo)
+                for linea in lector_csv:
+                    if user == linea[1] and password == linea[2]:
+                        QMessageBox.information(self, "Inicio de sesión", "Inicio de sesión exitoso")
+                        return True
+            QMessageBox.warning(self, "Inicio de sesión", "Credenciales inválidas, recuerde registrarse previamente antes de iniciar sesion")
+            return False
+        except FileNotFoundError:
+            QMessageBox.warning(self, "Inicio de sesión", "No se encontró el archivo de usuarios")
+            return False
+
+
+    def registrarse(self):
+        self.ventana('Registrarse')
+        self.repetir_contrasena = QLineEdit()
+        self.repetir_contrasena.setPlaceholderText("Repetir contraseña")
+        self.repetir_contrasena.setEchoMode(QLineEdit.Password)
+        self.layout_horizontal2.addWidget(self.repetir_contrasena)
+
+
     def iniciar_sesion(self):
-        print('Con este metodo iniciaremos sesion')
+        self.ventana('Iniciar sesión')
+        user = self.nombre_usuario.text()
+        password = self.contrasena.text()
+        if user and password:
+            if self.verificar_credenciales(user, password):
+                self.close()
