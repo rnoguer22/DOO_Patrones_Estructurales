f5c0ea30c466efb9d00b5c42dbdfa162b2b85cc1
rnoguer22 <rnogugon@myuax.com>
2023-11-26 20:43:27 +0100

    Primer prototipo de esta interfaz

--- Ejercicio2/Interface/interfaz_documentos.py
+++ Ejercicio2/Interface/interfaz_documentos.py
@@ -1,6 +1,7 @@
 import csv
-from PyQt5.QtWidgets import QWidget, QMessageBox, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit
-from Datos.guardar import Guardar
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QComboBox
+
 
 class Interface_Documents(QWidget):
 
@@ -25,3 +26,49 @@ class Interface_Documents(QWidget):
             self.crearArchivo()
         elif self.dialogo.clickedButton() == modificar_archivo:
             self.modificarArchivo()
+    
+
+    def crearArchivo(self):
+        #Creamos los layouts
+        layout_vertical = QVBoxLayout()
+        layout_horizontal1 = QHBoxLayout()
+        layout_horizontal2 = QHBoxLayout()
+        layout_horizontal3 = QHBoxLayout()
+
+        #Agregamos los elementos a los layouts
+        label_name = QLabel("Nombre del archivo: ")
+        name = QLineEdit()
+        name.setPlaceholderText("Nombre")
+        layout_horizontal1.addWidget(label_name)
+        layout_horizontal1.addWidget(name)
+
+        label_type = QLabel("Tipo de archivo: ")
+        tipos = {"Texto":".txt",
+                 "Imagen":".png", 
+                 "Video":".mp4", 
+                 "Audio":".mp3"}
+        tipo = QComboBox()
+        tipo.addItems(tipos.keys())
+        layout_horizontal2.addWidget(label_type)
+        layout_horizontal2.addWidget(tipo)
+
+        label_content = QLabel("Contenido del archivo: ")
+        content = QLineEdit()
+        content.setPlaceholderText("Contenido")
+        layout_horizontal3.addWidget(label_content)
+        layout_horizontal3.addWidget(content)
+
+        #Agregamos los layouts al layout vertical
+        layout_vertical.addLayout(layout_horizontal1)
+        layout_vertical.addLayout(layout_horizontal2)
+        layout_vertical.addLayout(layout_horizontal3)
+
+        self.setLayout(layout_vertical)
+
+        self.show()
+
+
+
+app = QApplication(sys.argv)
+interfaz = Interface_Documents()
+app.exec_()
